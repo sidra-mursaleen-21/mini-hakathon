5ea5f6b89ef29e882eda1257bba116c60f432f95
sidra mursaleen <sidramursaleen333@gmail.com>
2024-03-15 22:31:09 +0500

    hakathon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Router from "./config/router";
-import "./index.css";
-import theme from "./theme";
 
 function App() {
   return (
     <>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider>
         <Router />
       </ChakraProvider>
     </>
--- src/theme/index.jsx
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from '@chakra-ui/react'
-
-const theme = extendTheme({
-  body:{
-    backgroundColor:"#e8e8e8"
-  }
-})
-
-export default theme
